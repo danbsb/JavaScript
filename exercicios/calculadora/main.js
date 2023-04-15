cf55380f791181b12569811e342a35e61cdefe06
Daniel Fontinele <daniel.fontinele@gmail.com>
2023-04-14 23:54:59 -0300

    Calculadora concluída

--- exercicios/calculadora/main.js
+++ exercicios/calculadora/main.js
@@ -1,19 +1,100 @@
-let operacao = 0;
+let valorPrincipal = 0
+let valorSimbolo = null
+let valorSecundario = 0
 
-let valor = document.querySelector("#valorVisor");
-
-
-
-let botoes = document.querySelectorAll(".botao")
-botoes.forEach((botao) => {
-    botao.addEventListener("click", () => {
-        let contador = 0
-        let valorbutton = botao.value
-        if (valorbutton == "C") {
-            valor.innerHTML = 0;
-            console.log(operacao)
-        } else {
-            valor.innerHTML = valorbutton
+function visor(valorbutton) {
+    window.valorNumeros = Number(valorbutton.join(''))
+    if (valorPrincipal == 0) {
+        window.valorVisor = document.querySelector("#valorVisor");
+        valorVisor.innerHTML = valorbutton.join('')
+        if (isNaN(valorNumeros)) {
+            alert('Número inválido tente novamente')
+            valorVisor.innerHTML = 0
+            numVal = []
+            valorPrincipal = 0
+            valorSimbolo = null
+            valorSecundario = 0
         }
-    })
-});
+    } else {
+        window.valorVisor = document.querySelector("#valorVisor");
+        valorVisor.innerHTML = valorbutton.join('')
+        if (isNaN(valorNumeros)) {
+            alert('Número inválido tente novamente 2')
+            valorVisor.innerHTML = 0
+            numVal = []
+            valorPrincipal = 0
+            valorSimbolo = null
+            valorSecundario = 0
+        }
+        valorSecundario = valorNumeros
+    }
+
+}
+
+function numeros() {
+    let botoes = document.querySelectorAll(".number")
+    //função window antes da variável a torna global
+    window.numVal = []    
+    botoes.forEach((botao) => {
+        botao.addEventListener("click", () => {
+            let valorbutton = botao.value
+            numVal.push(valorbutton)
+            // console.log(numVal)
+            visor(numVal)
+        })
+    });
+}
+
+function simbolos() {
+    let botoes = document.querySelectorAll(".simbol")
+    botoes.forEach((botao) => {
+        botao.addEventListener("click", () => {
+            let valorSimbol = botao.value
+            valorVisor.innerHTML = valorSimbol
+            numVal = []
+            valorPrincipal = valorNumeros
+            if (valorSimbolo == null) {
+                valorSimbolo = valorSimbol
+            }
+        })
+    });
+}
+
+let contador = 0
+let botaoIgual = document.querySelector(".igual")
+botaoIgual.addEventListener("click", () => {
+        if(contador == 0) {
+            let n1 = valorPrincipal
+            let s1 = valorSimbolo
+            let n2 = valorSecundario
+            if(s1 == '+') {
+                let res1 = n1+n2
+                valorVisor.innerHTML = res1
+            } else if(s1 == '-') {
+                let res2 = n1-n2
+                valorVisor.innerHTML = res2
+            } else if(s1 == '÷') {
+                let res3 = n1/n2
+                valorVisor.innerHTML = res3
+            } else if(s1 == 'x') {
+                let res4 = n1*n2
+                valorVisor.innerHTML = res4
+            }
+            console.log(`${contador}`)
+            contador++
+            console.log(`${contador}`)
+            console.log(`${valorPrincipal}${valorSimbolo}${valorSecundario}`)
+        } else if(contador > 0) {
+            valorVisor.innerHTML = 0
+            numVal = []
+            valorPrincipal = 0
+            valorSimbolo = null
+            valorSecundario = 0
+            contador = 0
+        }
+    }    
+)
+
+
+numeros();
+simbolos();
---
+++ exercicios/calculadora/teste.js
@@ -0,0 +1,4 @@
+// const arrayDeNumeros = [1, 2, 3, 4, 5];
+// const numero = Number(arrayDeNumeros.join(''));
+
+// console.log(numero*2);
